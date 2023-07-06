f91726d55f1a298365f46e8ad6c19977baed73f8
volie <harunuyan6@gmail.com>
2023-07-06 13:59:18 +0300

    Added hilt application

---
+++ app/src/main/java/com/volie/instagramclonecompose/InstagramCloneComposeApplication.kt
@@ -0,0 +1,7 @@
+package com.volie.instagramclonecompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class InstagramCloneComposeApplication : Application()
